ebe91283039bc462d4246caf0f8024a4847f14fd
Nadim Edde Gomez <nadim@eddegomez.org>
2025-09-23 09:25:15 +0100

    feat(all): init repo

---
+++ README.md
@@ -0,0 +1,10 @@
+# Fjell
+
+Fjell is a minimal colorscheme based on [Mies](https://github.com/jaredgorski/Mies.vim) and [Mountain](https://github.com/mountain-theme/Mountain/blob/master/docs/fuji.org)
+
+## Design
+
+The theme simply uses `vim.api.nvim_set_hl` to set highlight colors and does not aim to provide any customisability.
+Color is only used for non-syntax elements such as LSP Diagnostics, search and menus.
+
+## Screenshot
---
+++ lua/fjell/colors.lua
@@ -0,0 +1,24 @@
+local colors = {}
+colors.dark = {
+	base00     = "#191919",
+	base01     = "#262626",
+	base02     = "#393939",
+	base03     = "#4C4C4C",
+	base04     = "#767676",
+	base05     = "#A0A0A0",
+	base06     = "#BFBFBF",
+    foreground = "#F0F0F0",
+	cyan       = "#8AABAA",
+	blue       = "#A5B4CB",
+	purple     = "#8F8AAC",
+	pink       = "#C49EC4",
+	green      = "#8AAC8B",
+	yellow     = "#C6A679",
+	red        = "#C4746E",
+	lightred   = "#C49EA0",
+
+	none = "NONE",
+	Selection = "#49483E",
+}
+
+return colors
